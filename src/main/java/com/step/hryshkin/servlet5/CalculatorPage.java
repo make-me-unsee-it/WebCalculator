35df4bf4d2a14575ae66c9b624838a053fe7faee
alekx <alekx-gr@yandex.by>
2022-06-17 20:10:53 +0300

    Initial commit

---
+++ src/main/java/com/step/hryshkin/servlet5/CalculatorPage.java
@@ -0,0 +1,21 @@
+package com.step.hryshkin.servlet5;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(urlPatterns = {"/calculator"})
+public class CalculatorPage extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        resp.setCharacterEncoding("UTF-8");
+
+        resp.getWriter().println("hhhh");
+    }
+
+}
